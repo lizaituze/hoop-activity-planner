b70121d95e67e6d7232cf7811936520edd40d746
Liza Ituze <lizaituze1@gmail.com>
2026-05-07 12:17:21 +0200

    I added hero DOM interactions

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ hamburgerBtn.addEventListener("click", function () {
 dropdownBtn.addEventListener("click", function () {
   dropdownMenu.classList.toggle("show");
 });
+const introText = document.getElementById("introText");
+const changeTextBtn = document.getElementById("changeTextBtn");
+const themeBtn = document.getElementById("themeBtn");
+
+changeTextBtn.addEventListener("click", function () {
+  introText.textContent =
+    "Consistency builds confidence. Keep training, keep improving, and keep showing up.";
+});
+
+themeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    themeBtn.textContent = "Light Mode";
+  } else {
+    themeBtn.textContent = "Dark Mode";
+  }
+});
